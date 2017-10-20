3833bc6fecc6806e92f7fccef34aea312d7b9de6
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2017-10-20 15:01:31 +0200

    Fixed sensor processing after sensor layout change.

--- greyhound/greyhound.go
+++ greyhound/greyhound.go
@@ -271,10 +271,40 @@ func positionBetweenSensors(value1 int, value2 int) int {
 	return (value1 - value2) * conf.SensorRadius / conf.SensorSpan
 }
 
+func hintCenter() {
+	nextPifLost = 0
+}
+func hintLeft() {
+	nextPifLost = -conf.SensorRadius * 4
+}
+func hintRight() {
+	nextPifLost = conf.SensorRadius * 4
+}
+func hintBetween(left int, right int) {
+	if right > left {
+		hintLeft()
+	} else if left > right {
+		hintRight()
+	} else {
+		hintCenter()
+	}
+}
+
+func sign(value int) int {
+	if value > 0 {
+		return 1
+	} else if value < 0 {
+		return -1
+	} else {
+		return 0
+	}
+}
+
 var currentP int
 var currentPTicks int
 var lastP int
 var lastPTicks int
+var nextPifLost int
 
 func processSensorData() {
 	read()
@@ -292,23 +322,26 @@ func processSensorData() {
 			// left - -
 			if isWhite(r) {
 				if isWhite(rr) {
-					// We lost the line, keep last direction
+					// We lost the line, use last hint
 					status = "----"
-					currentP = lastP
+					currentP = nextPifLost
 				} else {
 					// Far right
 					status = "---X"
-					currentP = (radius * 3) - distanceFromSensor(rr)
+					currentP = (radius * 3) + distanceFromSensor(rr)
+					hintRight()
 				}
 			} else {
 				if isWhite(rr) {
 					// Centered on r
 					status = "--X-"
 					currentP = radius - distanceFromSensor(r)
+					hintCenter()
 				} else {
 					// Angle (r, rr)
 					status = "--XX"
-					currentP = radius * 3
+					currentP = (radius * 2) + positionBetweenSensors(r, rr)
+					hintRight()
 				}
 			}
 		} else {
@@ -318,20 +351,24 @@ func processSensorData() {
 					// Centered on l
 					status = "-X--"
 					currentP = -radius + distanceFromSensor(l)
+					hintCenter()
 				} else {
 					// Inconclusive (l, rr), keep last value
 					status = "-X-X"
 					currentP = lastP
+					hintRight()
 				}
 			} else {
 				if isWhite(rr) {
 					// Between l and r
 					status = "-XX-"
 					currentP = lastP
+					hintBetween(r, l)
 				} else {
 					// Angle (l, r, rr)
 					status = "-XXX"
-					currentP = radius * 3
+					currentP = lastP
+					hintRight()
 				}
 			}
 		}
@@ -342,21 +379,25 @@ func processSensorData() {
 				if isWhite(rr) {
 					// Far left
 					status = "X---"
-					currentP = -(radius * 3) + distanceFromSensor(rr)
+					currentP = -(radius * 3) - distanceFromSensor(ll)
+					hintLeft()
 				} else {
 					// Inconclusive (ll, rr)
 					status = "X--X"
 					currentP = lastP
+					hintBetween(ll, rr)
 				}
 			} else {
 				if isWhite(rr) {
 					// Inconclusive (ll, r)
 					status = "X-X-"
 					currentP = lastP
+					hintLeft()
 				} else {
 					// Inconclusive (ll, r, rr)
 					status = "X-XX"
 					currentP = lastP
+					hintLeft()
 				}
 			}
 		} else {
@@ -365,21 +406,25 @@ func processSensorData() {
 				if isWhite(rr) {
 					// Angle (ll, l)
 					status = "XX--"
-					currentP = -(radius * 3)
+					currentP = -(radius * 2) + positionBetweenSensors(ll, l)
+					hintLeft()
 				} else {
 					// Inconclusive (ll, l, rr)
 					status = "XX-X"
 					currentP = lastP
+					hintLeft()
 				}
 			} else {
 				if isWhite(rr) {
 					// Angle (ll, l, r)
 					status = "XXX-"
-					currentP = -(radius * 3)
+					currentP = lastP
+					hintLeft()
 				} else {
 					// Inconclusive (ll, l, r, rr)
 					status = "XXXX"
 					currentP = lastP
+					hintBetween(ll, rr)
 				}
 			}
 		}
@@ -394,8 +439,8 @@ func processSensorData() {
 	deltaP := currentP - lastP
 	currentD := (conf.DTicksBoost * deltaP) / deltaTicks
 
-	factorP := ((currentP * conf.ParamP1) + (currentP * currentP * conf.ParamP2)) / conf.ParamPR
-	factorD := ((currentD * conf.ParamD1) + (currentD * currentD * conf.ParamD2)) / conf.ParamDR
+	factorP := ((currentP * conf.ParamP1) + (sign(currentP) * currentP * currentP * conf.ParamP2)) / conf.ParamPR
+	factorD := ((currentD * conf.ParamD1) + (sign(currentD) * currentD * currentD * conf.ParamD2)) / conf.ParamDR
 	steering := (factorP + factorD) / conf.InnerBrakeFactor
 
 	// print(status, ll, "[", sumSensor(cLL), "]", l, "[", sumSensor(cL), "]", r, "[", sumSensor(cR), "]", rr, "[", sumSensor(cRR), "]", "p", currentP, "dP", deltaP, "dP/dT", currentD, "dT", deltaTicks/1000, (currentPTicks-lastPTicks)/1000)
